2f7cec66ef76dc81227f4f2a615c4d2d31ec4da2
Friedrich Striewski <friedrich.striewski@bynder.com>
2018-11-21 14:37:12 +0100

    Ex4

--- learnrx - functional programming/ex3.js
+++ learnrx - functional programming/ex3.js
@@ -1,3 +1,9 @@
+// Projecting Arrays
+// Applying a function to a value and creating a new value is called a projection.To project one array into another, we apply a projection function to each item in the array and collect the results in a new array.
+
+// Exercise 3: Project an array of videos into an array of { id, title } pairs using forEach()
+// For each video, add a projected { id, title } pair to the videoAndTitlePairs array.
+
 function() {
   var newReleases = [
     {
